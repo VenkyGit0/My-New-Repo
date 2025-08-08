663ee35d1674f11a7957b276ec5da1ef6fd13137
Venkatesh <95609023+VenkyGit0@users.noreply.github.com>
2025-08-08 17:21:49 +0530

    Update new.java

--- new.java
+++ new.java
@@ -17,3 +17,21 @@ public class EvenOrOdd {
         input.close();
     }
 }
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) { // Check if the number is divisible by 2
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        input.close();
+    }
+}
