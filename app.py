78b3641087e25ae48c4e98fc7c872676a6f66e79
Venkatesh <95609023+VenkyGit0@users.noreply.github.com>
2025-08-08 13:59:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 This is we are writting for Jnekins test
 and also to automate the build
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints the string "Hello, World!" to the console
+    }
+}
